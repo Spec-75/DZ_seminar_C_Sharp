f9def1cf8e4d439dd121a5207c32544ef8c0a793
Spec-75 <Spec_75@mail.ru>
2022-06-25 16:49:14 +0300

    add zadacha_6

---
+++ zadacha_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(N);
+    Console.Write(" чётное.");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(N);
+    Console.Write(" не чётное.");
+}
