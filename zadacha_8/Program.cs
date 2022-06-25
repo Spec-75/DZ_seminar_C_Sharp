149612c826666f7c1077c7ebcca869afdbf67a48
Spec-75 <Spec_75@mail.ru>
2022-06-25 17:07:56 +0300

    add zadacha_8

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count <= N)
+{
+   if (count % 2 == 0)
+{
+    Console.WriteLine(count);
+}
+   count++; 
+}
+
